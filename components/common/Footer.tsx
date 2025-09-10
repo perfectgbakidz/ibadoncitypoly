183e09010e6906b6f94d9af15b47670df3a3f8a8
perfectgbakidz <122876271+perfectgbakidz@users.noreply.github.com>
2025-09-10 14:32:21 +0100

    Update Footer.tsx

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -7,8 +7,8 @@ const Footer: React.FC = () => {
     <footer className="bg-white border-t border-slate-200 text-slate-600 body-font z-20">
       <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
         <p className="text-sm text-slate-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-slate-200 sm:py-2 sm:mt-0 mt-4">
-          © 2024 Ibadan City Polytechnic —
-          <a href="https://twitter.com/polyibadan" className="text-slate-600 ml-1" rel="noopener noreferrer" target="_blank">@polyibadan</a>
+          © 2025 Moshood Abiola Polytechnic —
+          <a href="https://twitter.com/polyibadan" className="text-slate-600 ml-1" rel="noopener noreferrer" target="_blank">@mapoly</a>
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
             <Link to="/terms" className="text-sm text-slate-500 hover:text-green-600">Terms & Conditions</Link>
